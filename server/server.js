b8660959b62ef2b9c018939f8b99a999acb08615
James Luty <luty0125@gmail.com>
2022-05-17 10:55:15 -0700

    consolidated server

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ require("./config/mongoose.config");
 app.use(cors(), express.json(), express.urlencoded({ extended: true }));
 
 // This is where we import the products routes function from our product.routes.js file
-const AllProductRoutes = require("./routes/product.routes");
-AllProductRoutes(app);
+require("./routes/product.routes")(app);
 
 app.listen(PORT, () => console.log(`The server is all fired up on port ${PORT}`));
